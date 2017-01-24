6b201b35d3a59dfa879074f7838506e8535ea543
Yahweasel <no@email.no>
2017-01-24 02:00:25 +0000

    Import of the web page.

---
+++ index.php
@@ -0,0 +1,34 @@
+<?PHP
+$base = "/home/yahweasel/craig/rec";
+
+if (!isset($_REQUEST["id"]))
+    die("Hello.");
+$id = intval($_REQUEST["id"]);
+
+if (!file_exists("$base/$id.ogg.header1") ||
+    !file_exists("$base/$id.ogg.header2") ||
+    !file_exists("$base/$id.ogg.data"))
+    die("Invalid ID.");
+
+if (!isset($_REQUEST["fetch"])) {
+?>
+<!doctype html><html><head><title>Craig Records!</title></head><body>
+ID: <?PHP print $id; ?><br/>
+Download: <a href="?id=<?PHP print $id; ?>&fetch=cooked">processed</a>, <a href="?id=<?PHP print $id; ?>&fetch=raw">raw</a><br/>
+Note: Most audio editors will NOT support the raw version. Heck, some won't support the processed version.
+</body></html>
+<?PHP
+} else if ($_REQUEST["fetch"] === "cooked") {
+    header("Content-disposition: attachment; filename=$id.mkv");
+    header("Content-type: video/x-matroska");
+    passthru("/home/yahweasel/craig/cook.sh $id");
+
+} else {
+    header("Content-disposition: attachment; filename=$id.ogg");
+    header("Content-type: audio/ogg");
+    readfile("$base/$id.ogg.header1");
+    readfile("$base/$id.ogg.header2");
+    readfile("$base/$id.ogg.data");
+
+}
+?>
