729d6eeb109bad456ad0f30aba8d98a3a36815bf
Yahweasel <no@email.no>
2017-01-24 15:41:46 +0000

    Use the power of FIFOs to avoid temporary files per se.

--- cook.sh
+++ cook.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 set -e
 [ "$1" ]
+
 cd `dirname "$0"`/rec
+
 tmpdir=`mktemp -d`
+[ "$tmpdir" -a -d "$tmpdir" ]
+
 echo 'rm -rf '"$tmpdir" | at 'now + 2 hours'
+
 NB_STREAMS=`cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data | ffprobe -print_format flat -show_format - 2> /dev/null |
     grep '^format\.nb_streams' |
     sed 's/^[^=]*=//'`
 
+# Make all the fifos
+NICE="nice -n10 ionice -c3"
 for c in `seq 0 $((NB_STREAMS-1))`
 do
-    cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
-        nice -n10 ionice -c3 \
-        ffmpeg -codec libopus -copyts -i - \
+    mkfifo $tmpdir/$((c+1)).flac
+    $NICE cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+        $NICE ffmpeg -codec libopus -copyts -i - \
         -map 0:$c -af aresample=async=480,asyncts=first_pts=0 \
         -f wav - |
-        nice -n10 ionice -c3 \
-        flac - -o $tmpdir/$((c+1)).flac
+        $NICE flac - -c > $tmpdir/$((c+1)).flac &
 done
 
+# Zip them up
 cd $tmpdir
-zip -0 - *.flac
+zip -1 -FI - *.flac | cat
 cd
+
+# And clean up after ourselves
 rm -rf $tmpdir
