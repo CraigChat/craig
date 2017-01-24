9b5d59c04ef0f56b28d9c02864123f35c1615c7c
Yahweasel <no@email.no>
2017-01-24 01:59:53 +0000

    wavpack isn't portable. Use... ugh... raw PCM.

--- cook.sh
+++ cook.sh
@@ -6,4 +6,4 @@ cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
     nice -n10 ionice -c3 \
     ffmpeg -codec libopus -i - \
     -map 0 -af aresample=async=480 \
-    -c:a wavpack -f matroska -
+    -c:a pcm_s16le -f matroska -
