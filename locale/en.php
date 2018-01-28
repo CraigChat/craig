d9b648341fb432a863925f85a9dc7bcdbbb09623
Yahweasel <no@email.no>
2018-01-27 20:02:17 -0500

    First tiny step of locales.

---
+++ locale/en.php
@@ -0,0 +1,22 @@
+<?PHP
+$l["en"] = array(
+    "intro1" =>     "You may fetch your recording in various formats. The most useful formats, multi-track FLAC, Vorbis and AAC, are downloadable.",
+    "intro2" =>     "Other formats, including single-track mixed audio, require processing locally in your browser, and thus require a modern browser and some patience, and won't work well on mobile devices.",
+    "intro3" =>     "Your recording ID:",
+
+    "mtd" =>        "Multi-track download:",
+    "mtp" =>        "Multi-track processed:",
+    "stm" =>        "Single-track mixed:",
+
+    "uncomp" =>     "uncompressed",
+    "raw" =>        "Raw",
+    "rawnote" =>    "Note: Almost no audio editors will support this raw file",
+
+    "local" =>      "Local processing options",
+    "format" =>     "Format:",
+    "mix" =>        "Mix into single track (defeating Craig's entire purpose)",
+    "luddite" =>    "I am a luddite. I chose MP3 because I am ignorant, and I am unwilling to spend even a moment learning what the Hell I'm doing. I acknowledge that if I complain about the MP3 file this tool produces, or the abusiveness of this message, I will be banned. I am an imbecile, and I choose this option as a joyous expression of my own stupidity.",
+    "wav" =>        "Uncompressed audio is big, and this system processes audio directly into memory. I promise not to complain if anything goes wrong for that reason."
+    "begin" =>      "Begin processing"
+);
+?>
