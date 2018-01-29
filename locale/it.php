36cc510347f3bd9c63ad5ae1731aa0da5b430b53
Yahweasel <no@email.no>
2018-01-28 19:34:38 -0500

    Adding Italian translation.

---
+++ locale/it.php
@@ -0,0 +1,38 @@
+<?PHP
+$l["it"] = array(
+    "intro1" =>     "Puoi ottenere la tua registrazione in diversi formati. I formati più utili, FLAC multi-traccia, Vorbis e AAC, possono essere scaricati.",
+    "intro2" =>     "Gli altri formati, tra cui l'audio mixato in una singola traccia, richiedono un'elaborazione locale nel tuo browser, pertanto richiedono un browser moderno e un po' di pazienza, e la procedura non funziona bene sui dispositivi mobili.",
+    "intro3" =>     "L'ID della tua registrazione è:",
+
+    "mtd" =>        "Download multi-traccia:",
+    "mtp" =>        "Elaborazione multi-traccia:",
+    "stm" =>        "Elaborazione singola traccia:",
+
+    "uncomp" =>     "non compresso",
+    "raw" =>        "Raw",
+    "rawnote" =>    "Nota: Quasi nessun editor audio supporta questo file raw",
+
+    "local" =>      "Opzioni di elaborazione locale",
+    "format" =>     "Formato:",
+    "mix" =>        "Mixa in una traccia singola (rendendo inutile l'intero scopo di Craig)",
+    "luddite" =>    "Sono antiquato. Ho scelto MP3 perché sono ignorante, e non voglio dedicare nemmeno un attimo a capire che diamine sto facendo. Accetto di essere bannato se mi lamenterò del file MP3 prodotto da questo servizio, o della durezza di questo messaggio. Sono un imbecille, e scelgo questa opzione come gioisa espressione della mia stessa stupidità.",
+    "wav" =>        "L'audio non compresso è di grandi dimensioni, e questo sistema elabora l'audio direttamente in memoria. Prometto di non lamentarmi se qualcosa va storto per questo motivo.",
+    "begin" =>      "Inizia elaborazione",
+
+    "nomp3" =>      "Devi accettare le condizioni sui file MP3 prima di eseguire una conversione in MP3.",
+    "nowav" =>      "Devi accettare le condizioni sui file wav prima di eseguire una conversione in wav.",
+
+    "downloading" =>"Download in corso.",
+    "downfail" =>   "Download fallito!",
+    "notracks" =>   "Nessuna traccia trovata!",
+    "complete" =>   "Elaborazione completa.",
+
+    "delete1" =>    "Questo ELIMINERÀ la registrazione ",
+    "delete2" =>    "! Sei sicuro?",
+
+    "yes" =>        "Sì",
+    "no" =>         "No",
+
+    "deleted" =>    "Eliminata!"
+);
+?>
