1943bdaa89ea12a14e7aa62c5e6993c8804b1523
Yahweasel <no@email.no>
2018-08-09 16:30:16 -0400

    Adding support for multi-track leveling without mixing to cook.sh (not yet in the downloader)

--- cook.sh
+++ cook.sh
@@ -30,15 +30,36 @@ export PATH
 SCRIPTBASE=`dirname "$0"`
 SCRIPTBASE=`realpath "$SCRIPTBASE"`
 
+# Use cook.sh <ID> <format> <container> [dynaudnorm]
+
 [ "$1" ]
+ID="$1"
+shift
 
 FORMAT=flac
-[ "$2" ] && FORMAT="$2"
+[ "$1" ] && FORMAT="$1"
+shift
 
 CONTAINER=zip
-[ "$3" ] && CONTAINER="$3"
+[ "$1" ] && CONTAINER="$1"
+shift
 
 ARESAMPLE="aresample=flags=res:min_comp=0.001:max_soft_comp=0.025:min_hard_comp=15:first_pts=0"
+FILTER="$ARESAMPLE"
+
+for arg in "$@"
+do
+    case "$arg" in
+        dynaudnorm)
+            FILTER="$FILTER,dynaudnorm"
+            ;;
+
+        *)
+            printf 'Unrecognized argument "%s"\n' "$arg" >&2
+            exit 1
+            ;;
+    esac
+done
 
 ZIPFLAGS=-1
 EXTRAFILES=
@@ -127,16 +148,16 @@ mkdir "$OUTDIR" || exit 1
 if [ "$CONTAINER" = "aupzip" ]
 then
     # Put actual audio in the _data dir
-    OUTDIR="$OUTDIR/${1}_data"
+    OUTDIR="$OUTDIR/${ID}_data"
     mkdir "$OUTDIR" || exit 1
 fi
 
 # Take a lock on the data file so that we can detect active downloads
-exec 9< "$1.ogg.data"
+exec 9< "$ID.ogg.data"
 flock -s 9
 
 NICE="nice -n10 ionice -c3 chrt -i 0"
-NB_STREAMS=`timeout 10 cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+NB_STREAMS=`timeout 10 cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
     timeout 10 ffprobe - 2>&1 |
     grep 'Audio: opus' |
     wc -l`
@@ -169,31 +190,31 @@ fi
 if [ "$CONTAINER" = "aupzip" ]
 then
     (
-        sed 's/@PROJNAME@/'"$1"'_data/g' "$SCRIPTBASE/cook/aup-header.xml";
-        timeout $DEF_TIMEOUT cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+        sed 's/@PROJNAME@/'"$ID"'_data/g' "$SCRIPTBASE/cook/aup-header.xml";
+        timeout $DEF_TIMEOUT cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
             timeout $DEF_TIMEOUT "$SCRIPTBASE/cook/extnotes" -f audacity
-    ) > "$tmpdir/out/$1.aup"
+    ) > "$tmpdir/out/$ID.aup"
 fi
 
 # Encode thru fifos
 for c in `seq -w 1 $NB_STREAMS`
 do
-    O_USER="`$SCRIPTBASE/cook/userinfo.js $1 $c`"
+    O_USER="`$SCRIPTBASE/cook/userinfo.js $ID $c`"
     [ "$O_USER" ] || unset O_USER
     O_FN="$c${O_USER+-}$O_USER.$ext"
     O_FFN="$OUTDIR/$O_FN"
-    T_DURATION=`timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggduration" $c < $1.ogg.data`
+    T_DURATION=`timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggduration" $c < $ID.ogg.data`
     mkfifo "$O_FFN"
     if [ "$FORMAT" = "copy" -o "$CONTAINER" = "mix" ]
     then
-        timeout $DEF_TIMEOUT cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+        timeout $DEF_TIMEOUT cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
             timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggstender" $c > "$O_FFN" &
 
     else
-        timeout $DEF_TIMEOUT cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+        timeout $DEF_TIMEOUT cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
             timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggstender" $c |
             timeout $DEF_TIMEOUT $NICE ffmpeg -codec libopus -copyts -i - \
-            -af "$ARESAMPLE" \
+            -af "$FILTER" \
             -flags bitexact -f wav - |
             timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/wavduration" "$T_DURATION" |
             (
@@ -219,7 +240,7 @@ do
     if [ "$CONTAINER" = "aupzip" ]
     then
         printf '\t<import filename="%s" offset="0.00000000" mute="0" solo="0" height="150" minimized="0" gain="1.0" pan="0.0"/>\n' \
-            "$O_FN" >> "$tmpdir/out/$1.aup"
+            "$O_FN" >> "$tmpdir/out/$ID.aup"
     fi
 done
 if [ "$FORMAT" = "wavsfxm" -o "$FORMAT" = "wavsfxu" ]
@@ -228,16 +249,16 @@ then
 fi
 if [ "$CONTAINER" = "aupzip" ]
 then
-    printf '</project>\n' >> "$tmpdir/out/$1.aup"
+    printf '</project>\n' >> "$tmpdir/out/$ID.aup"
 fi
 if [ "$CONTAINER" = "zip" -o "$CONTAINER" = "aupzip" -o "$CONTAINER" = "exe" ]
 then
     mkfifo $OUTDIR/raw.dat
-    timeout 10 "$SCRIPTBASE/cook/recinfo.js" "$1" |
-        timeout $DEF_TIMEOUT cat - $1.ogg.header1 $1.ogg.header2 $1.ogg.data > $OUTDIR/raw.dat &
+    timeout 10 "$SCRIPTBASE/cook/recinfo.js" "$ID" |
+        timeout $DEF_TIMEOUT cat - $I$.ogg.header1 $ID.ogg.header2 $ID.ogg.data > $OUTDIR/raw.dat &
     (
-        timeout 10 "$SCRIPTBASE/cook/recinfo.js" "$1" text;
-        timeout $DEF_TIMEOUT cat $1.ogg.header1 $1.ogg.header2 $1.ogg.data |
+        timeout 10 "$SCRIPTBASE/cook/recinfo.js" "$ID" text;
+        timeout $DEF_TIMEOUT cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
             timeout $DEF_TIMEOUT "$SCRIPTBASE/cook/extnotes"
     ) > $OUTDIR/info.txt
 fi
@@ -278,7 +299,7 @@ case "$CONTAINER" in
         done
         MIXFILTER="$MIXFILTER amix=$c,dynaudnorm[aud]"
         FILTER="$FILTER$MIXFILTER"
-        DURATION=`timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggduration" < $1.ogg.data`
+        DURATION=`timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggduration" < $ID.ogg.data`
         timeout $DEF_TIMEOUT $NICE ffmpeg $INPUT -filter_complex "$FILTER" -map '[aud]' -flags bitexact -f wav - < /dev/null |
             timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/wavduration" "$DURATION" |
             (
@@ -293,7 +314,7 @@ case "$CONTAINER" in
         ;;
 
     aupzip)
-        timeout $DEF_TIMEOUT $NICE zip $ZIPFLAGS -r -FI - "$1.aup" "${1}_data"/*.$ext "${1}_data"/info.txt "${1}_data"/raw.dat
+        timeout $DEF_TIMEOUT $NICE zip $ZIPFLAGS -r -FI - "$ID.aup" "${ID}_data"/*.$ext "${ID}_data"/info.txt "${ID}_data"/raw.dat
         ;;
 
     *)
