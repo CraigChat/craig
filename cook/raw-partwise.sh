f4d437e07a883f942196d93036e09a1e780f3bb7
Yahweasel <no@email.no>
2019-03-28 21:04:05 -0400

    Program to generate the raw file partwise, which should help Ennuizel.

---
+++ cook/raw-partwise.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Copyright (c) 2019 Yahweasel
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+# OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+timeout() {
+    /usr/bin/timeout -k 5 "$@"
+}
+
+DEF_TIMEOUT=7200
+ulimit -v $(( 8 * 1024 * 1024 ))
+echo 10 > /proc/self/oom_adj
+
+PATH="/opt/node/bin:$PATH"
+export PATH
+
+SCRIPTBASE=`dirname "$0"`
+SCRIPTBASE=`realpath "$SCRIPTBASE/.."`
+
+# Use raw-partwise.sh <ID>
+
+[ "$1" ]
+ID="$1"
+
+cd "$SCRIPTBASE/rec"
+
+NICE="nice -n10 ionice -c3 chrt -i 0"
+STREAM_NOS=`timeout 10 "$SCRIPTBASE/cook/oggtracks" -n < $ID.ogg.header1`
+NB_STREAMS=`echo "$STREAM_NOS" | wc -l`
+
+# Output the recording info
+timeout 10 "$SCRIPTBASE/cook/recinfo.js" "$ID"
+
+# Output each component
+for c in `seq -w 1 $NB_STREAMS`
+do
+    timeout $DEF_TIMEOUT cat $ID.ogg.header1 $ID.ogg.header2 $ID.ogg.data |
+        timeout $DEF_TIMEOUT $NICE "$SCRIPTBASE/cook/oggstender" $c
+done
