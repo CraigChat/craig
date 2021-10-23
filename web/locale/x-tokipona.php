fc887a6bd6206fc6e52f86e88a8c3d1395dc0337
Yahweasel <git-rpo@yahweasel.com>
2021-10-23 23:52:59 +0000

    Updated Toki Pona

--- web/locale/x-tokipona.php
+++ web/locale/x-tokipona.php
@@ -1,8 +1,9 @@
 <?PHP
 $l["x-tokipona"] = array(
-    "intro1" =>     "mi awen e kalama sina lon lipu. sina ken kama jo e lipu kepeken lo ni. lipu kalama li ken kepeken nasin mute.",
+    "intro1" =>     "mi awen e kalama sina lon lipu. sina ken kama jo e lipu kepeken ilo ni. lipu kalama li ken kepeken nasin mute.",
     "intro1-updated" => 20180809,
     "intro2c" =>    "sitelen '*' li lon la nasin ni li wile e ni: ilo sina li pali e lipu.",
+    "intro2cbeta" =>"mi pini ala e ilo pali lipu ni; ona li pakala la o kepeken nasin ante.",
     "intro3" =>     "nanpa pi kalama awen sina:",
     "starttime" =>  "tenpo open",
     "users" =>      "jan",
