dab2b4581c7a2dc58f8132a1210c3e84bb2e4c28
Snazzah <suggesttosnazzy@gmail.com>
2022-01-05 17:22:05 -0600

    feat(web2): add cook route

--- web2/src/routes/cook.ts
+++ web2/src/routes/cook.ts
@@ -1,5 +1,5 @@
 import { RouteOptions } from 'fastify';
-import { getDuration } from '../util/cook';
+import { allowedContainers, allowedFormats, cook, getDuration, isReady } from '../util/cook';
 import { getRecording, keyMatches } from '../util/recording';
 
 export const durationRoute: RouteOptions = {
@@ -21,3 +21,75 @@ export const durationRoute: RouteOptions = {
     return reply.status(200).send({ ok: true, duration });
   }
 };
+
+export const getRoute: RouteOptions = {
+  method: 'GET',
+  url: '/api/recording/:id/cook',
+  handler: async (request, reply) => {
+    const { id } = request.params as Record<string, string>;
+    if (!id) return reply.status(400).send({ ok: false, error: 'Invalid ID' });
+    const { key } = request.query as Record<string, string>;
+    if (!key) return reply.status(403).send({ ok: false, error: 'Invalid key' });
+
+    const info = await getRecording(id);
+    if (info === false) return reply.status(410).send({ ok: false, error: 'Recording was deleted' });
+    else if (!info) return reply.status(404).send({ ok: false, error: 'Recording not found' });
+    if (!keyMatches(info, key)) return reply.status(403).send({ ok: false, error: 'Invalid key' });
+
+    const ready = await isReady(id);
+    return reply.status(200).send({ ok: true, ready });
+  }
+};
+
+export const postRoute: RouteOptions = {
+  method: 'POST',
+  url: '/api/recording/:id/cook',
+  handler: async (request, reply) => {
+    const { id } = request.params as Record<string, string>;
+    if (!id) return reply.status(400).send({ ok: false, error: 'Invalid ID' });
+    const { key } = request.query as Record<string, string>;
+    if (!key) return reply.status(403).send({ ok: false, error: 'Invalid key' });
+
+    const info = await getRecording(id);
+    if (info === false) return reply.status(410).send({ ok: false, error: 'Recording was deleted' });
+    else if (!info) return reply.status(404).send({ ok: false, error: 'Recording not found' });
+    if (!keyMatches(info, key)) return reply.status(403).send({ ok: false, error: 'Invalid key' });
+
+    const ready = await isReady(id);
+    if (!ready) return reply.status(429).send({ ok: false, error: 'This recording is already being processed' });
+
+    const body = request.body as { format?: string; container?: string; dynaudnorm?: boolean };
+    if (body.format && !allowedFormats.includes(body.format))
+      return reply.status(400).send({ ok: false, error: 'Invalid format' });
+    if (body.format === 'mp3' && !info.features.mp3)
+      return reply.status(403).send({ ok: false, error: 'This recording is missing the MP3 feature' });
+    const format = body.format || 'flac';
+
+    if (body.container && !Object.keys(allowedContainers).includes(body.container))
+      return reply.status(400).send({ ok: false, error: 'Invalid container' });
+    if (body.container === 'mix' && !info.features.mix)
+      return reply.status(403).send({ ok: false, error: 'This recording is missing the mix feature' });
+    const container = body.container || 'zip';
+
+    const dynaudnorm = Boolean(body.dynaudnorm);
+
+    try {
+      let ext = allowedContainers[container].ext || `${format}.zip`;
+      if (container === 'mix') ext = format === 'vorbis' ? 'ogg' : format;
+      const mime = allowedContainers[container].mime || 'application/zip';
+
+      const buffer = await cook(id, format, container, dynaudnorm);
+      return reply
+        .status(200)
+        .headers({
+          'content-disposition': `attachment; filename=${id}.${ext}`,
+          'content-type': mime
+        })
+        .send(buffer);
+    } catch (err) {
+      return reply.status(500).send({ ok: false, error: err.message });
+    }
+  }
+};
+
+// TODO add glowers support
--- web2/src/util/cook.ts
+++ web2/src/util/cook.ts
@@ -14,11 +14,65 @@ export async function isReady(id: string): Promise<boolean> {
     () => false
   );
   if (locked) return false;
-  return cooking.has(id);
+  return !cooking.has(id);
 }
 
 export async function getDuration(id: string): Promise<number> {
   const durationPath = path.join(cookPath, 'duration.sh');
   const { stdout: duration } = await execa(durationPath, [id]);
   return parseFloat(duration);
+}
+
+export const allowedFormats = [
+  'copy',
+  'oggflac',
+  'vorbis',
+  'aac',
+  'heaac',
+  'adpcm',
+  'wav8',
+  'opus',
+  'wavsfx',
+  'wavsfxm',
+  'wavsfxu',
+  'powersfx',
+  'powersfxm',
+  'powersfxu',
+  'ra'
+];
+
+export const allowedContainers: { [container: string]: { mime?: string; ext?: string } } = {
+  zip: {},
+  aupzip: {
+    ext: 'aup.zip'
+  },
+  ogg: {
+    ext: 'ogg',
+    mime: 'audio/ogg'
+  },
+  matroska: {
+    ext: 'mkv',
+    mime: 'video/x-matroska'
+  },
+  exe: {
+    ext: 'exe',
+    mime: 'application/vnd.microsoft.portable-executable'
+  },
+  mix: {
+    mime: 'application/octet-stream'
+  }
+};
+
+export async function cook(id: string, format = 'flac', container = 'zip', dynaudnorm = false): Promise<Buffer> {
+  try {
+    cooking.set(id, true);
+    const cookingPath = path.join(cookPath, '..', 'cook.sh');
+    const args = [id, format, container, dynaudnorm ? 'dynaudnorm' : ''];
+    const { stdout: cooked } = await execa(cookingPath, args);
+    cooking.delete(id);
+    return Buffer.from(cooked);
+  } catch (e) {
+    cooking.delete(id);
+    throw e;
+  }
 }
