96480264c688c2574f00e5a1436a932f13e074b5
Snazzah <suggesttosnazzy@gmail.com>
2022-01-12 10:44:31 -0600

    fix(web2): hide buttons properly

--- web2/page/src/components/recording.tsx
+++ web2/page/src/components/recording.tsx
@@ -80,13 +80,13 @@ export default function Recording({ state, onDurationClick, onDownloadClick }: R
       <Section title="Downloads" icon={downloadIcon}>
         {downloadsSection.map((section, i) => (<Section title={section.title} icon={section.icon} small key={i}>
           <div class="flex flex-row flex-wrap gap-3">
-            {section.buttons.map((button, ii) => <DownloadButton
+            {section.buttons.map((button, ii) => (button.hidden ? '' : <DownloadButton
               title={button.text}
               suffix={button.suffix}
               ennuizel={!!button.ennuizel}
               key={ii}
               onClick={(e) => onDownloadClick(button, e)}
-            />)}
+            />))}
           </div>
         </Section>))}
       </Section>
