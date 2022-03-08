b055fdb2979d791c824160d039fa9f08e567a079
Snazzah <suggesttosnazzy@gmail.com>
2022-03-08 02:00:11 -0600

    chore: move .env to root, add scripts

--- apps/bot/src/redis/index.ts
+++
@@ -1,7 +0,0 @@
-import Redis from 'ioredis';
-
-export const client = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
-  keyPrefix: 'craig:'
-});
