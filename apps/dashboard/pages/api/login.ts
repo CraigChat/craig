893e88981c41883751d80717e5aac9f0e288cab9
Snazzah <suggesttosnazzy@gmail.com>
2022-07-08 12:38:45 -0500

    fix(dashboard): use max age and strict samesite in setting cookies

--- apps/dashboard/pages/api/login.ts
+++ apps/dashboard/pages/api/login.ts
@@ -57,8 +57,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     serialize(config.cookieName, token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'lax',
-      path: '/'
+      sameSite: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7
     })
   );
 
