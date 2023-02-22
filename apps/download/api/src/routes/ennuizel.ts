856491c63f0ff431ce76df0cfce75639e1d8682d
Snazzah <me@snazzah.com>
2023-02-22 15:45:17 -0600

    fix(download): update ennuizel websocket function

--- apps/download/api/src/routes/ennuizel.ts
+++ apps/download/api/src/routes/ennuizel.ts
@@ -1,10 +1,13 @@
 import { captureException, withScope } from '@sentry/node';
 import destr from 'destr';
 import { RouteOptions } from 'fastify';
+import internal from 'node:stream';
 
 import { rawPartwise } from '../util/cook';
 import { getRecording, getUsers, keyMatches } from '../util/recording';
 import { timeoutWebsocket, toBuffer } from '../util/ws';
+
+const sendSize = 65536;
 
 export const ennuizelWebsocketRoute: RouteOptions = {
   method: 'GET',
@@ -40,15 +43,80 @@ export const ennuizelWebsocketRoute: RouteOptions = {
       const users = await getUsers(payload.i);
       if (!users[payload.t - 1]) return connection.socket.close(4003);
 
+      let stream: internal.Readable,
+        paused = false,
+        ackd = -1,
+        sending = 0,
+        buf = Buffer.alloc(4);
+      buf.writeUInt32LE(sending, 0);
+
+      function readable() {
+        if (paused) return;
+        let chunk;
+        while ((chunk = stream.read(sendSize))) {
+          setData(chunk);
+          if (paused) break;
+        }
+      }
+
+      function setData(chunk) {
+        buf = Buffer.concat([buf, chunk]);
+        while (buf.length >= sendSize) sendBuffer();
+      }
+
+      function sendBuffer() {
+        // Get the sendable part
+        let toSend;
+        if (buf.length > sendSize) {
+          toSend = buf.subarray(0, sendSize);
+          buf = buf.subarray(sendSize);
+        } else {
+          toSend = buf;
+          buf = null;
+        }
+
+        try {
+          connection.socket.send(toSend);
+        } catch (ex) {}
+
+        const hdr = Buffer.alloc(4);
+        sending++;
+        hdr.writeUInt32LE(sending, 0);
+        if (buf) buf = Buffer.concat([hdr, buf]);
+        else buf = hdr;
+
+        if (sending > ackd + 128) {
+          // Stop accepting data
+          paused = true;
+        }
+      }
+
+      connection.socket.on('message', (message) => {
+        const msg = toBuffer(message);
+        const cmd = msg.readUInt32LE(0);
+        const p = msg.readUInt32LE(4);
+        if (cmd !== 0) return connection.socket.close();
+        if (p > ackd) {
+          ackd = p;
+          if (sending <= ackd + 128) {
+            // Accept data
+            paused = false;
+            readable();
+          }
+        }
+      });
+
       try {
         connection.socket.send('{"ok":true}');
-        const stream = rawPartwise(payload.i, payload.t);
-        stream.on('data', (buf) => connection.socket.send(buf));
-        stream.once('end', () => connection.socket.close(1000));
-        stream.once('close', () => connection.socket.close(1000));
-        stream.once('error', () => connection.socket.close(4000));
-        connection.socket.once('message', () => {
-          connection.socket.close(4004);
+        stream = rawPartwise(payload.i, payload.t);
+        stream.on('readable', readable);
+        stream.once('end', () => {
+          while (buf.length > 4) sendBuffer();
+          sendBuffer();
+          connection.socket.close();
+        });
+        stream.once('close', () => {
+          connection.socket.close(1000);
           stream.destroy();
         });
       } catch (err) {
