aa757d70c4d923d82a40e1ef057e06bb84aebd9c
tomich <tomas.caram@gmail.com>
2024-05-20 15:34:56 -0300

    typo in sed again

--- start.sh
+++ start.sh
@@ -387,7 +387,7 @@ config_react(){
   # ----------------------------
   DOWNLOAD_DOMAIN=$(echo $API_HOMEPAGE|awk -F'://' '{print $2}')
   sed -z -E -i "s/(dexare:.*token:\s*)('')(.*applicationID:\s*)('')(.*downloadDomain:\s*)('localhost:5029')/\
-  \1'$DISCORD_BOT_TOKEN'\3'$DISCORD_APP_ID'\3'$DOWNLOAD_DOMAIN'/"\
+  \1'$DISCORD_BOT_TOKEN'\3'$DISCORD_APP_ID'\5'$DOWNLOAD_DOMAIN'/"\
   "$craig_dir/apps/bot/config/default.js"
 
 
