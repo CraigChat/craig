df4ce0da3aa290d956588b7bf3830f1c49a34367
Snazzah <me@snazzah.com>
2025-05-20 22:34:36 +0000

    fix(ferret): fix default directories

--- apps/ferret/src/lib/server/config.ts
+++ apps/ferret/src/lib/server/config.ts
@@ -7,9 +7,10 @@ import { env } from '$env/dynamic/private';
 
 export const debug = dev;
 
+const rootDir = dev ? '../../../../..' : '../../../../../..';
 export const KITCHEN_URL = env.KITCHEN_URL;
-export const REC_DIRECTORY = fileURLToPath(new URL(env.REC_DIRECTORY || '../../../../../rec', import.meta.url));
-export const DOWNLOADS_DIRECTORY = fileURLToPath(new URL(env.DOWNLOADS_DIRECTORY || '../../../../../downloads', import.meta.url));
+export const REC_DIRECTORY = fileURLToPath(new URL(env.REC_DIRECTORY || `${rootDir}/rec`, import.meta.url));
+export const DOWNLOADS_DIRECTORY = fileURLToPath(new URL(env.DOWNLOADS_DIRECTORY || `${rootDir}/downloads`, import.meta.url));
 
 export const REDIS_OPTIONS: RedisOptions = {
   host: env.REDIS_HOST || 'localhost',
