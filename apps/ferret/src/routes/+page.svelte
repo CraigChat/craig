2200c57fbee17268be7881bc4aa0ce7eec3e85bd
Snazzah <me@snazzah.com>
2025-05-27 19:54:42 +0000

    chore(ferret); update dev page

--- apps/ferret/src/routes/+page.svelte
+++ apps/ferret/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <section class="flex justify-center gap-2">
   {#if env.PUBLIC_TEST_RECORDINGS}
-    {#each env.PUBLIC_TEST_RECORDINGS.split(';') as pair}
+    {#each env.PUBLIC_TEST_RECORDINGS.split('|') as pair}
       {@const [id, key] = pair.split(':')}
       <a class="inline-block rounded bg-neutral-600 p-4" href={`/rec/${id}?key=${key}`}>view recording [{id}]</a>
     {/each}
