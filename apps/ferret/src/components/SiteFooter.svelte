b6071ff22d310f038cd321287e950930798ea3cb
Snazzah <me@snazzah.com>
2025-06-22 02:24:24 +0000

    feat(ferret): add privacy and tos links

--- apps/ferret/src/components/SiteFooter.svelte
+++ apps/ferret/src/components/SiteFooter.svelte
@@ -9,19 +9,26 @@
   import DynamicTranslatable from './DynamicTranslatable.svelte';
 </script>
 
+{#snippet supportServerLink()}
+  <a href="${SUPPORT_SERVER_URL}" target="_blank" class="underline underline-offset-2 transition-colors hover:text-blue-300">
+    {$t('footer.support_server')}
+  </a>
+{/snippet}
+
 <footer class="z-[1] inline-flex items-center text-xs sm:text-sm">
-  <div class="pb-5">
+  <div class="w-full pb-5">
     <p>{$t('footer.build')} <code>{version.slice(0, 7)}</code></p>
     <p class="text-blue-500">
       <span class="inline-block"><Icon icon={flaskIcon} class="translate-y-[0.125em]" /></span>
       <span>
-        <DynamicTranslatable
-          template={$t('footer.beta')}
-          replacements={{
-            support_server: { html: `<a href="${SUPPORT_SERVER_URL}" target="_blank" class="underline">${$t('footer.support_server')}</a>` }
-          }}
-        />
+        <DynamicTranslatable template={$t('footer.beta')} replacements={{ support_server: supportServerLink }} />
       </span>
     </p>
+    <hr class="my-2 border-white/25" />
+    <div>
+      <a href="https://craig.chat/privacy" class="underline underline-offset-2 transition-colors hover:text-white">{$t('footer.privacy')}</a>
+      •
+      <a href="https://craig.chat/terms" class="underline underline-offset-2 transition-colors hover:text-white">{$t('footer.tos')}</a>
+    </div>
   </div>
 </footer>
