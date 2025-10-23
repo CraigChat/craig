20a97c5d4548593f349ade910cd6fbc60f677ba6
Snazzah <me@snazzah.com>
2025-10-23 22:16:59 +0000

    chore: fix formatting issues

--- apps/ferret/vite.config.ts
+++ apps/ferret/vite.config.ts
@@ -1,14 +1,10 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-  plugins: [
-    enhancedImages(),
-    sveltekit(),
-    devtoolsJson()
-  ],
+  plugins: [enhancedImages(), sveltekit(), devtoolsJson()],
   server: {
     fs: { allow: ['../../locale'] },
     allowedHosts: ['.trycloudflare.com']
