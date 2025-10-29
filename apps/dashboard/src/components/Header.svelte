a508145866d2d5eada1208cb9d7ef45dad69270a
Snazzah <me@snazzah.com>
2025-10-29 17:03:10 +0000

    feat: add nightly/development badges to header

--- apps/dashboard/src/components/Header.svelte
+++ apps/dashboard/src/components/Header.svelte
@@ -2,13 +2,14 @@
   import Icon from '@iconify/svelte';
   import discordIcon from '@iconify-icons/simple-icons/discord';
 
-  import { SUPPORT_SERVER_URL } from '$lib/util';
+  import { env } from '$env/dynamic/public';
+  import { cn, SUPPORT_SERVER_URL } from '$lib/util';
 
   import LanguageButton from './LanguageButton.svelte';
 </script>
 
 <header class="absolute left-0 right-0 z-[1] mx-auto flex w-full max-w-4xl items-center justify-between p-2 sm:p-6">
-  <div class="font-display flex items-center justify-center gap-1 text-xl text-white sm:gap-2 sm:text-3xl">
+  <div class="font-display relative flex items-center justify-center gap-1 text-xl text-white sm:gap-2 sm:text-3xl">
     <a class="group flex items-center justify-center gap-2 sm:gap-3" href="https://craig.chat" target="_blank">
       <enhanced:img
         class="h-8 w-8 rounded-md sm:h-10 sm:w-10 sm:rounded-lg"
@@ -16,7 +17,19 @@
         alt="Craig"
         sizes="(min-width:640px) 40px, 32px"
       />
-      <div class="font-bold underline decoration-transparent transition-colors group-hover:decoration-teal-500">Craig</div>
+      <div class="font-bold underline decoration-transparent transition-colors group-hover:decoration-teal-500">
+        Craig
+        {#if env.PUBLIC_CRAIG_ENV === 'nightly' || env.PUBLIC_CRAIG_ENV === 'development'}
+          <span
+            class={cn(
+              'pointer-events-none absolute -bottom-2 right-0 rounded text-xs font-bold capitalize leading-none',
+              env.PUBLIC_CRAIG_ENV === 'nightly' ? 'text-purple-400' : 'text-red-400'
+            )}
+          >
+            {env.PUBLIC_CRAIG_ENV}
+          </span>
+        {/if}
+      </div>
     </a>
     <span>Dashboard</span>
   </div>
--- apps/ferret/src/components/SiteHeader.svelte
+++ apps/ferret/src/components/SiteHeader.svelte
@@ -1,14 +1,16 @@
 <script>
   import Icon from '@iconify/svelte';
   import discordIcon from '@iconify-icons/simple-icons/discord';
+  import clsx from 'clsx';
 
+  import { env } from '$env/dynamic/public';
   import { SUPPORT_SERVER_URL } from '$lib/util';
 
   import LanguageButton from './LanguageButton.svelte';
 </script>
 
 <header class="z-[1] inline-flex items-center justify-between">
-  <a class="group flex items-center justify-center gap-2 sm:gap-3" href="https://craig.chat" target="_blank">
+  <a class="group relative flex items-center justify-center gap-2 sm:gap-3" href="https://craig.chat" target="_blank">
     <enhanced:img
       class="h-8 w-8 rounded-md sm:h-10 sm:w-10 sm:rounded-lg"
       src="$assets/craig.png?w=32;40&aspect=1"
@@ -19,6 +21,16 @@
       class="font-display text-xl font-bold text-white underline decoration-transparent transition-colors group-hover:decoration-teal-500 sm:text-3xl"
     >
       Craig
+      {#if env.PUBLIC_CRAIG_ENV === 'nightly' || env.PUBLIC_CRAIG_ENV === 'development'}
+        <span
+          class={clsx(
+            'pointer-events-none absolute -bottom-2 right-0 rounded text-xs font-bold capitalize leading-none',
+            env.PUBLIC_CRAIG_ENV === 'nightly' ? 'text-purple-400' : 'text-red-400'
+          )}
+        >
+          {env.PUBLIC_CRAIG_ENV}
+        </span>
+      {/if}
     </div>
   </a>
   <div class="flex items-center gap-1">
