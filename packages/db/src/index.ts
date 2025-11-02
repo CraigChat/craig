1e16005a76520f96661934ab9b5168b44a1f6f8a
Snazzah <me@snazzah.com>
2025-11-02 22:27:03 +0000

    refactor: small refactor on db builting

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../.prisma-client/client.js';
 
 export const prisma = new PrismaClient();
 
-export * from '@prisma/client';
+export * from '../.prisma-client/client.js';
+export * from '../.prisma-client/models.js';
